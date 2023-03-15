7983b3fb21e36bf6c2e6ff837fa39b9adb9e1ece
Luiz Neto <luiznetopb@gmail.com>
2023-03-14 22:10:06 -0300

    Removendo questões

--- L01Q02/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    double valor;
-    cin >> valor;
-    cout << valor+(valor*0.55) << endl;
-}
--- L01Q05/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-int main() {
-  double cubo;
-  cin >> cubo;
-  cout << pow(cubo, 3) << endl;
-}
