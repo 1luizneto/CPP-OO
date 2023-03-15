06dfc0ca2a81f19017342976b5bc8c51862892bc
Luiz Neto <luiznetopb@gmail.com>
2023-03-14 21:57:07 -0300

    Lista 01 Questão 07

---
+++ L01Q07/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double matricula, salarioB, deducao, salarioL;
+  char nome[100];
+
+  cin >> matricula;
+  cin >> nome;
+  cin >> salarioB;
+
+  deducao = salarioB*0.15;
+  salarioL = salarioB - deducao;
+
+  cout << "Matrícula: " << mmatricula << "\nNome: " <<
+}
